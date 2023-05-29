af3d4d32470e5af42fc2a81dcf5a8f62ce9f6a7e
andresortegac <andres.ortegac97@gmail.com>
2023-05-28 22:08:54 -0500

    Ocultar botón copiar al correr el proyecto

--- Script.js
+++ Script.js
@@ -1,40 +1,50 @@
 const textArea = document.querySelector('.text-area');
 const mensaje = document.querySelector('.mensaje');
+const notificaciones = document.querySelector('.notificaciones');
+const btnCopiar = document.querySelector('.btn-copiar');
+const btnEncriptar = document.querySelector('.btn-encriptar');
+const btnDesencriptar = document.querySelector('.btn-desencriptar');
 
-function btnEncriptar(){
+btnEncriptar.addEventListener('click', function() {
     const textoEncriptado = encriptar(textArea.value);
-    mensaje.value = textoEncriptado
-    textArea.value= "";
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
     mensaje.style.backgroundImage = "none";
-    const notificaciones = document.querySelector('.notificaciones');
     notificaciones.style.display = "none";
+    mostrarBotonCopiar();
+});
+
+btnDesencriptar.addEventListener('click', function() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+    notificaciones.style.display = "none";
+    mostrarBotonCopiar();
+});
+
+function mostrarBotonCopiar() {
+    btnCopiar.style.display = 'block';
 }
 
-function encriptar(stringEncriptado){
+function encriptar(stringEncriptado) {
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringEncriptado =stringEncriptado.toLowerCase();
+    stringEncriptado = stringEncriptado.toLowerCase();
 
-    for(let i=0; i<matrizCodigo.length;i++) {
-        if(stringEncriptado.includes(matrizCodigo[i][0])){
-            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]); 
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]); 
         }
     }
     return stringEncriptado;
 }
 
-function btnDesencriptar(){
-    const textoDesencriptado = desencriptar(textArea.value);
-    mensaje.value = textoDesencriptado
-    textArea.value= "";
-}
-
-function desencriptar(stringDesncriptado){
+function desencriptar(stringDesncriptado) {
     let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringDesncriptado =stringDesncriptado.toLowerCase();
+    stringDesncriptado = stringDesncriptado.toLowerCase();
 
-    for(let i=0; i<matrizCodigo.length;i++) {
-        if(stringDesncriptado.includes(matrizCodigo[i][1])){
-            stringDesncriptado = stringDesncriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]); 
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesncriptado.includes(matrizCodigo[i][1])) {
+            stringDesncriptado = stringDesncriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]); 
         }
     }
     return stringDesncriptado;
@@ -45,5 +55,3 @@ function copiarTexto() {
     document.execCommand('copy');
     alert('Texto copiado al portapapeles.');
 }
-
-
